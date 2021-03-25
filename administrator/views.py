0a365ba5191f45a7b2369062f8f07f8f914e48ef
lezdegamo <lezly.degamo@gmail.com>
2021-03-25 18:32:08 +0800

    View all users

--- administrator/views.py
+++ administrator/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from user.models import *
 from organizer.models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 class AdministratorDashboardView(View):
@@ -14,7 +15,15 @@ class AdministratorDashboardView(View):
 
 class AdministratorUsersView(View):
   def get(self, request):
-    return render(request, 'user.html')
+    qs_users = User.objects.all()
+    qs_organizers = Organizer.objects.all()
+    qs_administrators = Administrator.objects.all()
+    context = {
+      'users' : qs_users,
+      'organizers' : qs_organizers,
+      'administrators': qs_administrators
+    }
+    return render(request, 'user.html', context)
 
 class AdministratorEventsView(View):
   def get(self, request):
