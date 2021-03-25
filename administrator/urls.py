7ef6d61071c412fe59c45f8dce4decd9ad1680b6
lezdegamo <lezly.degamo@gmail.com>
2021-03-25 17:28:38 +0800

    Update urls.py

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	path('dashboard', views.AdministratorDashboardView.as_view(), name="administrator_dashboard_view"),
   path('users', views.AdministratorUsersView.as_view(), name="administrator_users_view"),
   path('events', views.AdministratorEventsView.as_view(), name="administrator_events_view"),
-  path('manage', views.AdministratorEventsView.as_view(), name="administrator_manage_view"),
+  path('manage', views.AdministratorManageView.as_view(), name="administrator_manage_view"),
 ] 
